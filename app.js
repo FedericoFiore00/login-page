6e0a9a26899286bcfe82dcc4be65cb575c18e5fa
FedericoFiore00 <federicofiore71@gmail.com>
2025-10-16 22:26:55 +0200

    aggiunto loader

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-const btnSignup = document.querySelector('.blackCard .noAccount .buttonBlack');
+
+const box = document.querySelector('.box');    
+const btnSignup = document.querySelector('.noAccount .buttonBlack'); 
+const btnLogin = document.querySelector('.loginAccount .buttonBlack'); 
+const btnsubmitLogIn = document.querySelector('.buttonWhitelog');
+const btnsubmitsign = document.querySelector('.buttonWhitesign');
+
+btnSignup.addEventListener('click', () => {
+    box.classList.add('is-signup');   
+});
+
+btnLogin.addEventListener('click', () => {
+    box.classList.remove('is-signup');
+});
+
+btnsubmitLogIn.addEventListener('click', () => {
+    console.log('Log In button clicked');
+});
+
